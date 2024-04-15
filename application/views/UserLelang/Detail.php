6a6c14d29579b3b7c5b9789b53060bed8c1edb5c
Your Name <stephenmalik86@gmail.com>
2024-04-15 20:03:23 +0700

    selesai ke-2

--- application/views/UserLelang/Detail.php
+++ application/views/UserLelang/Detail.php
@@ -202,48 +202,37 @@
       else
         button.disabled = true;
 
-      function myFunction() {
-        var checkbox = document.getElementById("myCheck");
-        const button = document.getElementById('button');
-        if (checkbox.checked == true)
-          button.disabled = false;
-        else
-          button.disabled = true;
 
+    }
 
-      }
+    // Mengatur waktu akhir perhitungan mundur
+    var countDownDate = new Date("<?= date('d F Y', strtotime($barang['tgl_jaminan'])) ?>").getTime();
 
-      // Mengatur waktu akhir perhitungan mundur
-      var countDownDate = new Date("<?= date('d F Y', strtotime($barang['tgl_jaminan'])) ?>").getTime();
-      // Mengatur waktu akhir perhitungan mundur
-      var countDownDate = new Date("<?= date('d F Y', strtotime($barang['tgl_jaminan'])) ?>").getTime();
+    // Memperbarui hitungan mundur setiap 1 detik
+    var x = setInterval(function() {
 
-      // Memperbarui hitungan mundur setiap 1 detik
-      var x = setInterval(function() {
-            // Memperbarui hitungan mundur setiap 1 detik
-            var x = setInterval(function() {
+        // Untuk mendapatkan tanggal dan waktu hari ini
+        var now = new Date().getTime();
 
-              // Untuk mendapatkan tanggal dan waktu hari ini
-              var now = new Date().getTime();
+        // Temukan jarak antara sekarang dan tanggal hitung mundur
+        var distance = countDownDate - now;
 
-              // Temukan jarak antara sekarang dan tanggal hitung mundur
-              var distance = countDownDate - now;
+        // Perhitungan waktu untuk hari, jam, menit dan detik
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-              // Perhitungan waktu untuk hari, jam, menit dan detik
-              var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-              var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-              var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-              var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        // Keluarkan hasil dalam elemen dengan id = "demo"
+        document.getElementById("demo").innerHTML = days + "h " + hours + "j " +
+          minutes + "m " + seconds + "s ";
 
-              // Keluarkan hasil dalam elemen dengan id = "demo"
-              document.getElementById("demo").innerHTML = days + "h " + hours + "j " +
-                minutes + "m " + seconds + "s ";
-
-              // Jika hitungan mundur selesai, tulis beberapa teks 
-              if (distance < 0) {
-                clearInterval(x);
-                document.getElementById("demo").innerHTML = "Jaminan Telah Ditutup";
-                document.getElementById("btnModal").disabled = true;
-              }
-            }, 1000);
+        // Jika hitungan mundur selesai, tulis beberapa teks 
+        if (distance < 0) {
+          clearInterval(x);
+          document.getElementById("demo").innerHTML = "Jaminan Telah Ditutup";
+          document.getElementById("btnModal").disabled = true;
+        }
+      },
+      1000);
   </script>
